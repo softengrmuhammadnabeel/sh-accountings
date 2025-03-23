91d91088e0001fceab37f42b6bb1769e0359e78e
Muhammad Nabeel <se.muhammadnabeel@gmail.com>
2025-03-23 18:04:13 +0500

    feat: added video

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -71,48 +71,139 @@
 
 // export default Header
 
+// "use client";
+// import { assets } from "@/assets/assets";
+// import { assetsVid } from "@/assets/mp4";
+// import * as React from "react";
+// import Box from "@mui/material/Box";
+// import Container from "@mui/material/Container";
+// import Typography from "@mui/material/Typography";
+// import Button from "@mui/material/Button";
+// import Image from "next/image";
+// import { motion } from "framer-motion";
+
+// export const Header = () => {
+//     const images = [assets.acc1, assets.acc2, assets.acc3];
+//     const [currentImageIndex, setCurrentImageIndex] = React.useState(0);
+
+//     React.useEffect(() => {
+//         const interval = setInterval(() => {
+//             setCurrentImageIndex((prevIndex) => (prevIndex + 1) % images.length);
+//         }, 2000);
+//         return () => clearInterval(interval);
+//     }, []);
+
+//     return (
+//         <Box sx={{ width: "100%", height: "100vh", position: "relative" }}>
+//             {images.map((image, index) => (
+//                 <motion.div
+//                     key={index}
+//                     initial={{ opacity: 0 }}
+//                     animate={{ opacity: index === currentImageIndex ? 1 : 0, scale: index === currentImageIndex ? 1.05 : 1 }}
+//                     transition={{ duration: 1, ease: "easeInOut" }}
+//                     style={{
+//                         position: "absolute",
+//                         top: 0,
+//                         objectFit:"cover",
+//                         left: 0,
+//                         width: "100%",
+//                         height: "100%",
+//                     }}
+//                 >
+//                     <Image src={image} alt={`Background ${index + 1}`} layout="fill" objectFit="cover" quality={85} priority={index === 0} />
+//                     <Box sx={{ position: "absolute", top: 0, left: 0, width: "100%", height: "100%", backgroundColor: "rgba(0, 0, 0, 0.5)" }} />
+//                 </motion.div>
+//             ))}
+
+//             <Container
+//                 maxWidth="lg"
+//                 sx={{
+//                     position: "relative",
+//                     zIndex: 1,
+//                     display: "flex",
+//                     flexDirection: "column",
+//                     alignItems: "center",
+//                     justifyContent: "center",
+//                     height: "100%",
+//                     color: "common.white",
+//                     textAlign: "center",
+
+//                 }}
+//             >
+//                 <Typography
+//                     variant="h1"
+//                     component={motion.h1}
+//                     initial={{ opacity: 0, y: -20 }}
+//                     animate={{ opacity: 1, y: 0 }}
+//                     transition={{ duration: 0.8 }}
+//                     sx={{ mt: 20, mb: 3, fontWeight: 700, fontSize: { xs: "2.5rem", sm: "3.5rem", md: "4.5rem" }, lineHeight: 1.2 }}
+//                 >
+//                     Manage Your <Typography component="span" color="#EB1552" variant="inherit">Finances</Typography> With Confidence!
+//                 </Typography>
+
+//                 <Typography
+//                     variant="h6"
+//                     component={motion.p}
+//                     initial={{ opacity: 0, y: 10 }}
+//                     animate={{ opacity: 1, y: 0 }}
+//                     transition={{ duration: 0.9 }}
+//                     sx={{
+//                         mb: 4,
+//                         fontSize: { xs: "1rem", sm: "1.25rem" },
+//                         color: "rgba(255, 255, 255, 0.8)",
+//                         maxWidth: "800px",
+//                         mx: "auto",
+//                     }}
+//                 >
+//                     We provide expert accounting, tax, and financial services to help businesses and individuals make informed decisions and achieve financial success.
+//                 </Typography>
+
+//                 <Button
+//                     variant="contained"
+//                     size="large"
+//                     component={motion.button}
+//                     initial={{ scale: 0.9, opacity: 0 }}
+//                     animate={{ scale: 1, opacity: 1 }}
+//                     transition={{ duration: 0.6, ease: "easeInOut" }}
+//                     sx={{ backgroundColor:"#EB1552",fontSize: "1rem", fontWeight: 600, px: 4, py: 2, borderRadius: "8px", boxShadow: "none", "&:hover": { backgroundColor: "primary.dark" } }}
+//                 >
+//                     Contact Us
+//                 </Button>
+//             </Container>
+//         </Box>
+//     );
+// };
+
+
 "use client";
-import { assets } from "@/assets/assets";
 import * as React from "react";
 import Box from "@mui/material/Box";
 import Container from "@mui/material/Container";
 import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
-import Image from "next/image";
 import { motion } from "framer-motion";
 
 export const Header = () => {
-    const images = [assets.acc1, assets.acc2, assets.acc3];
-    const [currentImageIndex, setCurrentImageIndex] = React.useState(0);
-
-    React.useEffect(() => {
-        const interval = setInterval(() => {
-            setCurrentImageIndex((prevIndex) => (prevIndex + 1) % images.length);
-        }, 2000);
-        return () => clearInterval(interval);
-    }, []);
-
     return (
-        <Box sx={{ width: "100%", height: "100vh", position: "relative" }}>
-            {images.map((image, index) => (
-                <motion.div
-                    key={index}
-                    initial={{ opacity: 0 }}
-                    animate={{ opacity: index === currentImageIndex ? 1 : 0, scale: index === currentImageIndex ? 1.05 : 1 }}
-                    transition={{ duration: 1, ease: "easeInOut" }}
-                    style={{
-                        position: "absolute",
-                        top: 0,
-                        objectFit:"cover",
-                        left: 0,
-                        width: "100%",
-                        height: "100%",
-                    }}
-                >
-                    <Image src={image} alt={`Background ${index + 1}`} layout="fill" objectFit="cover" quality={85} priority={index === 0} />
-                    <Box sx={{ position: "absolute", top: 0, left: 0, width: "100%", height: "100%", backgroundColor: "rgba(0, 0, 0, 0.5)" }} />
-                </motion.div>
-            ))}
+        <Box sx={{ width: "100%", height: "100vh", position: "relative", overflow: "hidden" }}>
+            <video
+                autoPlay
+                loop
+                muted
+                playsInline
+                style={{
+                    position: "absolute",
+                    top: 0,
+                    left: 0,
+                    width: "100%",
+                    height: "100%",
+                    objectFit: "cover",
+                }}
+            >
+                <source src={"/vid.mp4"} type="video/mp4" />
+            </video>
+
+            <Box sx={{ position: "absolute", top: 0, left: 0, width: "100%", height: "100%", backgroundColor: "rgba(0, 0, 0, 0.5)" }} />
 
             <Container
                 maxWidth="lg"
@@ -126,7 +217,6 @@ export const Header = () => {
                     height: "100%",
                     color: "common.white",
                     textAlign: "center",
-
                 }}
             >
                 <Typography
@@ -152,6 +242,7 @@ export const Header = () => {
                         color: "rgba(255, 255, 255, 0.8)",
                         maxWidth: "800px",
                         mx: "auto",
+                        
                     }}
                 >
                     We provide expert accounting, tax, and financial services to help businesses and individuals make informed decisions and achieve financial success.
@@ -164,7 +255,7 @@ export const Header = () => {
                     initial={{ scale: 0.9, opacity: 0 }}
                     animate={{ scale: 1, opacity: 1 }}
                     transition={{ duration: 0.6, ease: "easeInOut" }}
-                    sx={{ backgroundColor:"#EB1552",fontSize: "1rem", fontWeight: 600, px: 4, py: 2, borderRadius: "8px", boxShadow: "none", "&:hover": { backgroundColor: "primary.dark" } }}
+                    sx={{ backgroundColor: "#EB1552", fontSize: "1rem", fontWeight: 600, px: 4, py: 2, borderRadius: "8px", boxShadow: "none", "&:hover": { backgroundColor: "#3c0e63" } }}
                 >
                     Contact Us
                 </Button>
