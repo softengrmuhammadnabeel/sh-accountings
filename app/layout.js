4a9cb3f8da517fbd6870a233e8dcd7a4b7d8420d
Muhammad Nabeel <se.muhammadnabeel@gmail.com>
2025-04-13 16:39:44 +0500

    feat: added favico tab icons

--- app/layout.js
+++ app/layout.js
@@ -8,16 +8,19 @@ const outfit = Outfit({
 const ovo = Ovo({
   subsets: ["latin"], weight: ["400"]
 });
-
-
 export const metadata = {
-  title: "Clear Horizon",
+  title: "Clear Horizon Accountant",
   description: "Developed by MD Nabeel",
   icons: {
-    icon: "/navbar/3-tab.png",
-    shortcut: "/navbar/3-tab.png",
-    apple: "/navbar/3-tab.png",
-  }
+    icon: [
+      { url: "/tab/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+      { url: "/tab/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+      { url: "/tab/android-chrome-192x192.png", sizes: "192x192", type: "image/png" },
+      { url: "/tab/android-chrome-512x512.png", sizes: "512x512", type: "image/png" },
+    ],
+    apple: "/tab/apple-touch-icon.png",
+    shortcut: "/tab/favicon.ico",
+  },
 };
 
 
